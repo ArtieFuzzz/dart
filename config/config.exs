ac23afa4d2c3c1d1a8569fd69601976eaf712807
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-06-24 10:19:20 +1000

    MongoDB
    For storing pastes

--- config/config.exs
+++ config/config.exs
@@ -3,4 +3,5 @@ import Config
 config :dart,
   port: String.to_integer(System.get_env("DART_PORT") || "7701"),
   tenor_key: System.get_env("DART_TENOR_APIKEY"),
-  redis_uri: System.get_env("DART_REDIS_URI")
+  redis_uri: System.get_env("DART_REDIS_URI"),
+  mongo_uri: System.get_env("DART_MONGO_URI")
--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule Dart.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:httpoison, "~> 1.8"},
       {:jason, "~> 1.3"},
-      {:redix, "~> 1.1"}
+      {:redix, "~> 1.1"},
+      {:mongodb, "~> 0.5.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
